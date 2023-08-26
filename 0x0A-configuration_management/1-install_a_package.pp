0a626f55247a016451eee1029a48483b77b204e8
Oweis-Nd <oweilayefandoni@gmail.com>
2023-08-26 06:19:19 -0700

    Configuration manag

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet to install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
