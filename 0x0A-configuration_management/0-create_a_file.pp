facfb27c02b066833e4836b74a0f425f458dc255
Oweis-Nd <oweilayefandoni@gmail.com>
2023-08-26 06:11:47 -0700

    Configuration manag

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# This manifest creates a file at /tmp
-file { '/tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
- content => 'I love Puppet',
-}
+# Create file in /tmp
+
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
